d78215a0823cde7cb1fbc18cb35d1714d516362d
Joshua Obritsch <j.obritsch@gmail.com>
2019-11-15 17:36:22 +0100

    Add _itemsTransformer to StoriesBloc

--- lib/src/blocs/stories_bloc.dart
+++ lib/src/blocs/stories_bloc.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:rxdart/rxdart.dart';
 import '../models/item_model.dart';
 import '../resources/repository.dart';
@@ -5,8 +6,17 @@ import '../resources/repository.dart';
 class StoriesBloc {
   final _repository = Repository();
   final _topIds = PublishSubject<List<int>>();
+  final _items = BehaviorSubject<int>();
+
+  Observable<Map<int, Future<ItemModel>>> items;
 
   Observable<List<int>> get topIds => _topIds.stream;
+
+  Function(int) get fetchItem => _items.sink.add;
+
+  StoriesBloc() {
+    _items.stream.transform(_itemsTransformer());
+  }
 
   fetchTopIds() async {
     final ids = await _repository.fetchTopIds();
@@ -14,7 +24,19 @@ class StoriesBloc {
     _topIds.sink.add(ids);
   }
 
+  _itemsTransformer() {
+    return ScanStreamTransformer(
+      (Map<int, Future<ItemModel>> cache, int id, _) {
+        cache[id] = _repository.fetchItem(id);
+
+        return cache;
+      },
+      <int, Future<ItemModel>>{},
+    );
+  }
+
   void dispose() {
     _topIds.close();
+    _items.close();
   }
 }
